3452d28f03c7ba16f1c7a93809dd9f9c9982e2d8
Luna Simons <luna@bddvlpr.com>
2026-03-13 14:02:12 +0100

    fix: depend runner on forgejo

--- systems/rena/impl/forgejo-runner.nix
+++ systems/rena/impl/forgejo-runner.nix
@@ -10,10 +10,16 @@
       url = "https://git.avali.network";
       tokenFile = config.sops.secrets."forgejo/runner-token".path;
       labels = [
-        "ubuntu-latest:docker://node:25-bookworm"
-        "ubuntu-24.04:docker://node:25-bookworm"
-        "ubuntu-22.04:docker://node:25-bookworm"
+        "ubuntu-latest:docker://catthehacker/ubuntu:act-latest"
+        "ubuntu-24.04:docker://catthehacker/ubuntu:act-24.04"
+        "ubuntu-22.04:docker://catthehacker/ubuntu:act-22.04"
+        "ubuntu-20.04:docker://catthehacker/ubuntu:act-20.04"
       ];
     };
+  };
+
+  systemd.services.gitea-runner-rena = {
+    after = [ "forgejo.service" ];
+    requires = [ "forgejo.service" ];
   };
 }
