e9226664cb98fb6743365d6bf7b2e3ae93503bc7
Luna Simons <luna@bddvlpr.com>
2026-03-22 21:04:51 +0100

    chore: flake update

--- systems/rena/impl/jackboxresoniteproxy.nix
+++ systems/rena/impl/jackboxresoniteproxy.nix
@@ -6,4 +6,14 @@
     enable = true;
     openFirewall = true;
   };
+
+  services.nginx.virtualHosts."jackbox.avali.network" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:3002";
+      proxyWebsockets = true;
+    };
+    locations."= /".return = "302 https://avali.network/";
+  };
 }
