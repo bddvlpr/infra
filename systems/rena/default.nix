f4ee131116335b2be2e07d2f44f4a05a0550102a
Luna Simons <luna@bddvlpr.com>
2026-03-22 20:50:59 +0100

    chore: temporary redirect to forgejo

--- systems/rena/default.nix
+++ systems/rena/default.nix
@@ -15,5 +15,12 @@
     MAILADDR luna@bddvlpr.com
   '';
 
+  # Temporarily redirect any traffic to the forgejo instance.
+  services.nginx.virtualHosts."avali.network" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."/".return = "302 https://git.avali.network/";
+  };
+
   system.stateVersion = "25.11";
 }
